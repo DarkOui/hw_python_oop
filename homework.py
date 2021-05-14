2f46cf7b5262def2c06aec372dc837e71a434226
Алексей Ерасов <darkoui.acc@gmail.com>
2021-05-14 10:37:42 +0300

    overdone4!

--- homework.py
+++ homework.py
@@ -29,7 +29,7 @@ class Calculator:
         """Метод расчета потраченного сегодня."""
         today = dt.date.today()
         today_amount = sum(record.amount for record in self.records
-                            if record.date == today)
+                           if record.date == today)
         return today_amount
 
     def get_week_stats(self):
@@ -37,7 +37,7 @@ class Calculator:
         today = dt.date.today()
         week_ago = today - dt.timedelta(days=7)
         week_amount = sum(record.amount for record in self.records
-                                 if week_ago < record.date <= today)
+                          if week_ago < record.date <= today)
         return week_amount
 
     def get_remained(self):
@@ -79,25 +79,3 @@ class CaloriesCalculator(Calculator):
             return ('Сегодня можно съесть что-нибудь ещё, но с '
                     f'общей калорийностью не более {calories_remained} кКал')
         return 'Хватит есть!'
-
-
-# Проверки
-cash_calculator = CashCalculator(1000)
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-cash_calculator.add_record(Record(amount=3000,
-                               comment='бар в Танин др', date='09.05.2021'))
-print(cash_calculator.get_today_cash_remained('rub'))
-#На сегодня осталось 555 руб
-calcM = CashCalculator(10000)
-calcC = CaloriesCalculator(10000)
-calcM.add_record(Record(amount=145.6, comment='Безудержный шопинг'))
-calcM.add_record(Record(amount=1568.9,
-                       comment='Наполнение потребительской корзины'))
-calcM.add_record(Record(amount=691.4, comment='Катание на такси'))
-calcC.add_record(Record(amount=1186, comment='Кусок тортика. И ещё один.'))
-calcC.add_record(Record(amount=84, comment='Йогурт.'))
-calcC.add_record(Record(amount=1140.1, comment='Баночка чипсов.'))
-print(calcM.get_today_cash_remained('evr'))
-print(calcM.get_week_stats())
-print(calcC.get_calories_remained())
