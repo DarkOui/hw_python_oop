e5e76057c57ad9d0635774666b2c4ec485826f41
Алексей Ерасов <darkoui.acc@gmail.com>
2021-05-14 10:35:54 +0300

    overdone3!

--- homework.py
+++ homework.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 
 class Record:
-    """Преобразование данных в списке Calculator.records"""
+    """Преобразование данных в списке Calculator.records."""
 
     def __init__(self, amount, comment, date=None):
         self.amount = amount
@@ -27,25 +27,17 @@ class Calculator:
 
     def get_today_stats(self):
         """Метод расчета потраченного сегодня."""
-        # today_amount = []
         today = dt.date.today()
-        today_amount = sum([record.amount for record in self.records
-                            if record.date == today])
-        # for record in self.records:
-        #   if record.date == today:
-        #       today_amount.append(record.amount)
+        today_amount = sum(record.amount for record in self.records
+                            if record.date == today)
         return today_amount
 
     def get_week_stats(self):
         """Метод расчета, сколько денег потрачено за последние 7 дней."""
-        # week_amount = 0
         today = dt.date.today()
         week_ago = today - dt.timedelta(days=7)
-        week_amount = float(sum([record.amount for record in self.records
-                                 if week_ago < record.date <= today]))
-        # for record in self.records:
-        #    if week_ago < record.date <= today:
-        #        week_amount += record.amount
+        week_amount = sum(record.amount for record in self.records
+                                 if week_ago < record.date <= today)
         return week_amount
 
     def get_remained(self):
@@ -61,19 +53,20 @@ class CashCalculator(Calculator):
 
     def get_today_cash_remained(self, currency='rub'):
         """Метод расчета, сколько денег еще можно потратить."""
-        currencies = {'usd': ('USD', self.USD_RATE),
-                      'eur': ('Euro', self.EURO_RATE),
-                      'rub': ('руб', self.RUB_RATE)}
         cash_remained = self.get_remained()
         if cash_remained == 0:
             return 'Денег нет, держись'
-        if currency not in currencies.keys():
+        currencies = {'usd': ('USD', self.USD_RATE),
+                      'eur': ('Euro', self.EURO_RATE),
+                      'rub': ('руб', self.RUB_RATE)}
+        if currency not in currencies:
             return 'Нет такой валюты'
         currency_name, currency_rate = currencies[currency]
         money = round(cash_remained / currency_rate, 2)
+        amoney = abs(money)
         if cash_remained > 0:
             return f'На сегодня осталось {money} {currency_name}'
-        return f'Денег нет, держись: твой долг - {abs(money)} {currency_name}'
+        return f'Денег нет, держись: твой долг - {amoney} {currency_name}'
 
 
 class CaloriesCalculator(Calculator):
@@ -83,28 +76,28 @@ class CaloriesCalculator(Calculator):
         """Метод расчета, сколько ещё калорий можно/нужно получить сегодня."""
         calories_remained = self.get_remained()
         if calories_remained > 0:
-            return (f'Сегодня можно съесть что-нибудь ещё, но с '
+            return ('Сегодня можно съесть что-нибудь ещё, но с '
                     f'общей калорийностью не более {calories_remained} кКал')
         return 'Хватит есть!'
 
 
 # Проверки
-# cash_calculator = CashCalculator(1000)
-# cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# cash_calculator.add_record(Record(amount=3000,
-#                                comment='бар в Танин др', date='09.05.2021'))
-# print(cash_calculator.get_today_cash_remained('rub'))
-# На сегодня осталось 555 руб
-# calcM = CashCalculator(10000)
-# calcC = CaloriesCalculator(1000)
-# calcM.add_record(Record(amount=145.6, comment='Безудержный шопинг'))
-# calcM.add_record(Record(amount=1568.9,
-#                        comment='Наполнение потребительской корзины'))
-# calcM.add_record(Record(amount=691.4, comment='Катание на такси'))
-# calcC.add_record(Record(amount=1186, comment='Кусок тортика. И ещё один.'))
-# calcC.add_record(Record(amount=84, comment='Йогурт.'))
-# calcC.add_record(Record(amount=1140.1, comment='Баночка чипсов.'))
-# print(calcM.get_today_cash_remained('evr'))
-# print(calcM.get_week_stats())
-# print(calcC.get_calories_remained())
+cash_calculator = CashCalculator(1000)
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+cash_calculator.add_record(Record(amount=3000,
+                               comment='бар в Танин др', date='09.05.2021'))
+print(cash_calculator.get_today_cash_remained('rub'))
+#На сегодня осталось 555 руб
+calcM = CashCalculator(10000)
+calcC = CaloriesCalculator(10000)
+calcM.add_record(Record(amount=145.6, comment='Безудержный шопинг'))
+calcM.add_record(Record(amount=1568.9,
+                       comment='Наполнение потребительской корзины'))
+calcM.add_record(Record(amount=691.4, comment='Катание на такси'))
+calcC.add_record(Record(amount=1186, comment='Кусок тортика. И ещё один.'))
+calcC.add_record(Record(amount=84, comment='Йогурт.'))
+calcC.add_record(Record(amount=1140.1, comment='Баночка чипсов.'))
+print(calcM.get_today_cash_remained('evr'))
+print(calcM.get_week_stats())
+print(calcC.get_calories_remained())
